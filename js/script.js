d0033bd1312e322169f562efe6da6d96407b96f0
Sasha Leal <sashaleal@Sashas-MacBook-Air.local>
2022-07-06 23:09:29 -0500

    Add an Async Function step 3

--- js/script.js
+++ js/script.js
@@ -13,8 +13,10 @@ const guessedLetters = [];
 let remainingGuesses = 8;
 
 const getWord = async function(){
-const response = await fetch ("“https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt”")
+const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
 const words = await response.text();
+const wordArray = words.split("\n");
+console.log(wordArray)
 }
 
 getWord();
