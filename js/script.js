dafe370e35e04f49976bab086144f73cf71b3527
Sasha Leal <sashaleal@Sashas-MacBook-Air.local>
2022-07-06 23:17:03 -0500

    Add an Async Function step 5

--- js/script.js
+++ js/script.js
@@ -16,7 +16,9 @@ const getWord = async function(){
 const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
 const words = await response.text();
 const wordArray = words.split("\n");
-console.log(wordArray)
+//console.log(wordArray)
+const randomIndex =  Math.floor(Math.random() * wordArray.length);
+word= wordArray[randomIndex].trim();
 }
 
 getWord();
